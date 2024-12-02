3bf7fe7a8ba1cf066b5c84657d508286e4c69d3a
Jan De Poorter <jan@defv.be>
2024-12-02 21:24:16 +0100

    Day 2, part 2
    Feels like a brute-force solution though...

--- day02-reports/src/main.rs
+++ day02-reports/src/main.rs
@@ -14,14 +14,31 @@ impl From<&str> for Report {
 }
 
 impl Report {
-    fn is_safe(&self) -> bool {
-        let differences: Vec<i32> = self.levels
-            .windows(2)
-            .map(|window| window[1] - window[0])
-            .collect();
+    fn str_is_safe(levels: &Vec<i32>) -> bool {
+        let differences: Vec<i32> = levels
+        .windows(2)
+        .map(|window| window[1] - window[0])
+        .collect();
 
         differences.iter().all(|&x| (x > 0 && x < 4)) ||
             differences.iter().all(|&x| (x < 0 && x > -4))
+    }
+
+    fn is_safe(&self) -> bool {
+        return Report::str_is_safe(&self.levels);
+    }
+
+    fn is_dampened_safe(&self) -> bool {
+        if Report::str_is_safe(&self.levels) {
+            return true;
+        }
+
+        self.levels.iter().enumerate().any(|(i, _)| {
+            let mut levels = self.levels.clone();
+            levels.remove(i);
+
+            Report::str_is_safe(&levels)
+        })
     }
 }
 
@@ -29,11 +46,19 @@ fn main() {
     let document = aoc::input();
 
     println!("Step 1: {}", step1(&document));
+    println!("Step 2: {}", step2(&document));
 }
 
 fn step1(input: &str) -> String {
     input.lines()
          .filter(|line| Report::from(*line).is_safe())
+         .count()
+         .to_string()
+}
+
+fn step2(input: &str) -> String {
+    input.lines()
+         .filter(|line| Report::from(*line).is_dampened_safe())
          .count()
          .to_string()
 }
@@ -55,6 +80,11 @@ mod tests {
         assert_eq!(step1(EXAMPLE), "2");
     }
 
+    #[test]
+    fn test_step2() {
+        assert_eq!(step2(EXAMPLE), "4");
+    }
+
 
     #[test]
     fn test_is_safe() {
@@ -62,5 +92,14 @@ mod tests {
         assert!(Report::from("7 6 4 2 1").is_safe());
         assert!(!Report::from("1 2 7 8 9").is_safe());
         assert!(!Report::from("1 3 2 4 5").is_safe());
+    }
+
+    #[test]
+    fn test_is_dampened_safe() {
+        assert!(Report::from("7 6 4 2 1").is_dampened_safe());
+        assert!(Report::from("1 3 2 4 5").is_dampened_safe());
+        assert!(Report::from("8 6 4 4 1").is_dampened_safe());
+        assert!(!Report::from("1 2 7 8 9").is_dampened_safe());
+        assert!(!Report::from("9 7 6 2 1").is_dampened_safe());
     }
 }
