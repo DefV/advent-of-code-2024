47f1908d8e7108bf5bcd7d46e023ae386b8972bc
Jan De Poorter <jan@defv.be>
2024-12-04 22:50:38 +0100

    Part 2. Not the cleanest solution sure, but it got it done

--- day04-wordsearch/src/main.rs
+++ day04-wordsearch/src/main.rs
@@ -30,6 +30,30 @@ impl Puzzle {
             .count() as u32
     }
 
+    // Just hardcode we're looking for the MAS cross
+    fn cross_count(&self) -> u32 {
+        (0..self.grid.len())
+            .flat_map(|x| (0..self.grid[x].len()).map(move |y| (x, y)))
+            .filter(|&(x, y)| self.char_at(x as isize, y as isize) == Some('A'))
+            .filter(|&(x, y)| {
+                let (x, y) = (x as isize, y as isize);
+                let lt = self.char_at(x - 1, y - 1);
+                let rt = self.char_at(x + 1, y - 1);
+                let lb = self.char_at(x - 1, y + 1);
+                let rb = self.char_at(x + 1, y + 1);
+
+
+                match (lt, rb, lb, rt) {
+                    (Some('M'), Some('S'), Some('M'), Some('S')) => true,
+                    (Some('S'), Some('M'), Some('S'), Some('M')) => true,
+                    (Some('M'), Some('S'), Some('S'), Some('M')) => true,
+                    (Some('S'), Some('M'), Some('M'), Some('S')) => true,
+                    _ => false,
+                }
+            })
+            .count() as u32
+    }
+
     fn words_from(&self, x: isize, y: isize, len: usize) -> Vec<String> {
         DIRECTIONS.iter().map(|(dx, dy)| {
             let mut letters = vec![];
@@ -57,10 +81,15 @@ fn main() {
     let input = aoc::input();
 
     println!("Step 1 result: {}", step1(&input));
+    println!("Step 2 result: {}", step2(&input));
 }
 
 fn step1(input: &str) -> u32 {
     Puzzle::from(input).word_count("XMAS")
+}
+
+fn step2(input: &str) -> u32 {
+    Puzzle::from(input).cross_count()
 }
 
 #[cfg(test)]
@@ -80,5 +109,10 @@ MXMXAXMASX";
     #[test]
     fn step1_example() {
         assert_eq!(super::step1(EXAMPLE), 18);
+    }
+
+    #[test]
+    fn step2_example() {
+        assert_eq!(super::step2(EXAMPLE), 9);
     }
 }
