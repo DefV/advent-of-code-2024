c86f6851b1db2c5267df8354b9716bf8345ab9b3
Jan De Poorter <jan@defv.be>
2024-12-06 22:51:28 +0100

    Day 6, step 2
    Now to find out how smarter people then me solve this elegantly

--- day06-guardsguardsguards/src/main.rs
+++ day06-guardsguardsguards/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq,Hash)]
 enum Direction {
     North,
     East,
@@ -51,19 +51,19 @@ impl Map {
 
     }
     fn step(&mut self) -> bool {
-        let (x, y, _) = self.position;
-        let (next_x, next_y) = match self.position.2 {
-            Direction::North => (x - 1, y),
-            Direction::East => (x, y + 1),
-            Direction::South => (x + 1, y),
-            Direction::West => (x, y - 1),
+        let (x, y, direction) = self.position;
+        let (next_x, next_y) = match direction {
+            Direction::North => (x as isize - 1, y as isize),
+            Direction::East => (x as isize, y as isize + 1),
+            Direction::South => (x as isize + 1, y as isize),
+            Direction::West => (x as isize, y as isize - 1),
         };
 
         if let Some(obstructed) = self.at(next_x, next_y) {
             if *obstructed {
-                self.position = (x, y, Self::turn_right(&self.position.2));
+                self.position = (x, y, Self::turn_right(&direction));
             } else {
-                self.position = (next_x, next_y, self.position.2);
+                self.position = (next_x as usize, next_y as usize, direction);
             }
 
             true
@@ -72,8 +72,21 @@ impl Map {
         }
     }
 
-    fn at(&self, x: usize, y: usize) -> Option<&bool> {
-        self.data.get(x)?.get(y)
+    fn is_loop(&mut self) -> bool {
+        let mut visited_positions: HashSet<(usize, usize, Direction)> = HashSet::new();
+        visited_positions.insert(self.position);
+
+        while self.step() {
+            if !visited_positions.insert(self.position) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn at(&self, x: isize, y: isize) -> Option<&bool> {
+        self.data.get(x as usize)?.get(y as usize)
     }
 }
 
@@ -81,6 +94,7 @@ fn main() {
     let input = aoc::input();
 
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> i32 {
@@ -93,6 +107,30 @@ fn part1(input: &str) -> i32 {
     }
 
     visited_positions.len() as i32
+}
+
+fn part2(input: &str) -> i32 {
+    let mut visited_positions: HashSet<(usize, usize)> = HashSet::new();
+    let mut map = Map::from(input);
+    let start = map.position.clone();
+
+    while map.step() {
+        visited_positions.insert((map.position.0, map.position.1));
+    }
+
+    // Let's see if we get a loop by putting something in the way on every visited position
+    visited_positions.into_iter()
+        .filter_map(|(x, y)| {
+            let mut data = map.data.clone();
+            data[x][y] = true;
+            let mut map = Map { data, position: start };
+            if map.is_loop() {
+                Some((x, y))
+            } else {
+                None
+            }
+        })
+        .count() as i32
 }
 
 #[cfg(test)]
@@ -115,5 +153,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(EXAMPLE), 41);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE), 6);
     }
 }
