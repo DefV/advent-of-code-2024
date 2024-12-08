47f1dbf2cae003216e8e763e070a66754dd92b0b
Jan De Poorter <jan@defv.be>
2024-12-08 20:47:52 +0100

    Day 8, part 2
    Turned out to be quite easy

--- day08-antennas/src/main.rs
+++ day08-antennas/src/main.rs
@@ -11,13 +11,14 @@ enum Point {
 struct Map {
     points: Vec<Vec<Point>>,
     antennas: HashMap<char, Vec<Location>>,
+    bounds: (usize, usize),
 }
 
 impl From<&str> for Map {
     fn from(input: &str) -> Self {
         let mut antennas = HashMap::new();
 
-        let points = input
+        let points: Vec<Vec<Point>> = input
             .lines()
             .enumerate()
             .map(|(x, line)| {
@@ -36,7 +37,9 @@ impl From<&str> for Map {
             })
             .collect();
 
-        Map { points, antennas }
+        let bounds = (points.len(), points[0].len());
+
+        Map { points, antennas, bounds }
     }
 }
 
@@ -45,17 +48,17 @@ impl Map {
         self.points.get(x)?.get(y)
     }
 
-    fn antinodes_for(&self, antennas: &[Location]) -> Vec<Location>{
-        let combinations: Vec<(Location, Location)> = antennas.iter().flat_map(|antenna| {
+    fn combinations(&self, antennas: &[Location]) -> Vec<(Location, Location)> {
+        antennas.iter().flat_map(|antenna| {
             antennas
                 .iter()
                 .filter(move |&other| other != antenna)
                 .map(|other| (*antenna, *other))
-        }).collect();
+        }).collect()
+    }
 
-        // The combination of antennas loops every pair twice, in each direction
-        // So we only need to add one direction
-        combinations.iter().filter_map(|((x1, y1), (x2, y2))| {
+    fn antinodes_for(&self, antennas: &[Location]) -> Vec<Location>{
+        self.combinations(antennas).iter().filter_map(|((x1, y1), (x2, y2))| {
             Some((
                 ((*x1 * 2) as isize - *x2 as isize).try_into().ok()?,
                 ((*y1 * 2) as isize - *y2 as isize).try_into().ok()?
@@ -63,14 +66,38 @@ impl Map {
         }).collect()
     }
 
-    fn antinodes(&self) -> Vec<Location> {
+    fn repeated_antinodes_for(&self, antennas: &[Location]) -> Vec<Location> {
+        let mut antinodes = vec![];
+        for (antenna, other) in self.combinations(antennas) {
+            let mut n = 0;
+            loop {
+                let x = antenna.0 as isize + (antenna.0 as isize - other.0 as isize) * n;
+                let y = antenna.1 as isize + (antenna.1 as isize - other.1 as isize) * n;
+
+                if x < 0 || y < 0 || x >= self.bounds.0 as isize || y >= self.bounds.1 as isize {
+                    break;
+                }
+
+                antinodes.push((x as usize, y as usize));
+                n += 1;
+            }
+        }
+
+        antinodes
+    }
+
+    fn antinodes(&self, repeatable: bool) -> Vec<Location> {
         self.antennas.values().flat_map(|antennas| {
-            self.antinodes_for(antennas)
+            if repeatable {
+                self.repeated_antinodes_for(antennas)
+            } else {
+                self.antinodes_for(antennas)
+            }
         }).collect()
     }
 
-    fn placeable_antinodes(&self) -> HashSet<Location> {
-        self.antinodes().iter().filter(|(x, y)| {
+    fn placeable_antinodes(&self, repeatable: bool) -> HashSet<Location> {
+        self.antinodes(repeatable).iter().filter(|(x, y)| {
             self.point_at(*x, *y).is_some()
         }).cloned().collect()
     }
@@ -79,10 +106,10 @@ impl Map {
 fn main() {
     let input = aoc::input();
 
-    println!("Part 1: {}", part1(&input));
-}
+    let map = Map::from(input.as_str());
+    let part1_result = map.placeable_antinodes(false).len();
+    println!("Part 1: {}", part1_result);
 
-fn part1(input: &str) -> usize {
-    let map = Map::from(input);
-    map.placeable_antinodes().len()
+    let part2_result = map.placeable_antinodes(true).len();
+    println!("Part 2: {}", part2_result);
 }
