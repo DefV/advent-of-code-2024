12dbf26f0a7116f19d705918c3f0de5d523644eb
Jan De Poorter <jan@defv.be>
2024-12-13 21:29:29 +0100

    Day 12, Part 2... I was on to you AoC :p

--- day13-the-claw/src/main.rs
+++ day13-the-claw/src/main.rs
@@ -3,28 +3,31 @@ use regex::Regex;
 // Button A: X+94, Y+34
 // Button B: X+22, Y+67
 // Prize: X=8400, Y=5400
-fn solve_machine(input: &str) -> Option<u32> {
+fn solve_machine(input: &str, multiplicator: u64) -> Option<u64> {
     let regex = Regex::new(
         r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)",
     )
     .unwrap();
     let captures = regex.captures(input).unwrap();
 
-    let numbers: Vec<f32> = captures
+    let numbers: Vec<f64> = captures
         .iter()
         .skip(1)
-        .map(|x| x.unwrap().as_str().parse::<f32>().unwrap())
+        .map(|x| x.unwrap().as_str().parse::<f64>().unwrap())
         .collect();
 
     let [x1, y1, x2, y2, r1, r2] = numbers[..] else {
         panic!("Invalid input")
     };
 
+    let r1 = r1 + multiplicator as f64;
+    let r2 = r2 + multiplicator as f64;
+
     let times_a = ((r1*y2) - (r2*x2)) / ((x1*y2) - (y1*x2));
     let times_b = ((r1*y1) - (r2*x1)) / ((x2*y1) - (y2*x1));
 
     if (times_a.round() == times_a) && (times_b.round() == times_b) {
-        Some((times_a * 3f32 + times_b * 1f32) as u32)
+        Some((times_a * 3f64 + times_b * 1f64) as u64)
     } else {
         None
     }
@@ -32,9 +35,11 @@ fn solve_machine(input: &str) -> Option<u32> {
 
 fn main() {
     let input = aoc::input();
-    let result: u32 = input.split("\n\n").filter_map(|machine| solve_machine(machine)).sum();
+    let part1_result: u64 = input.split("\n\n").filter_map(|machine| solve_machine(machine, 0)).sum();
+    let part2_result: u64 = input.split("\n\n").filter_map(|machine| solve_machine(machine, 10000000000000)).sum();
 
-    println!("Part 1: {}", result);
+    println!("Part 1: {}", part1_result);
+    println!("Part 2: {}", part2_result);
 }
 
 #[cfg(test)]
